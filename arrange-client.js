69d66f76dd83fbb025aed18e120425f21d752511
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-27 20:20:08 +0000

    Deploy arrangeplatform/client-library to github.com/arrangeplatform/client-library.git:gh-pages

--- arrange-client.js
+++ arrange-client.js
@@ -1,12 +1,10 @@
-/* global io */
-
 /**
- * Arrange client library
+ * Arrange client library. The instanziation of a new server connection is done via Arrange.connect(url).
  */
 class Arrange {
 
     /**
-     * Connect to server.
+     * Connect to server. Use Arrange.connect() for easier access.
      * @param {String} url URL of the server, defaults to window.location at same host
      */
     constructor(url) {
@@ -14,8 +12,30 @@ class Arrange {
     }
 
     /**
-     * Creates a client and returns it with an id. When the name contains invalid characters
-     * (all except lower letters), null is returned.
+     * Create a parent child relation between two objects.
+     * The parent and child objects must exist in the client of the logged in user.
+     * @param {String} parentdatatypeid Datatype ID of the parent object
+     * @param {String} parentobjectid ID of the parent object
+     * @param {String} childdatatypeid Datatype ID of the child object
+     * @param {String} childobjectid ID of the child object
+     * @returns {Boolean} True, when the parent child relation was created, false otherwise
+     */
+    async addchild(parentdatatypeid, parentobjectid, childdatatypeid, childobjectid) {
+        return this.dorequest('addchild', { parentdatatypeid: parentdatatypeid, parentobjectid: parentobjectid, childdatatypeid: childdatatypeid, childobjectid: childobjectid }, 'onaddchild');
+    }
+
+    /**
+     * Creates a client and returns it with an id.
+     * Works only, when logged in as 'admin'.
+     * @param {String} clientname Name for the new client. Does not need to be unique and can contain any string.
+     * @returns Client object containing an id field or null when the logged in user was not 'admin'.
+     * @example
+     * await arrangeconnection.login('admin', 'adminpassword'); // Need to login as admin
+     * var newclient = await Arrange.createClient('My new client');
+     * // newclient = {
+     * //     id: 'generatedid',
+     * //     name: 'My new client'
+     * // }
      */
     async createclient(clientname) {
         return this.dorequest('createclient', { clientname: clientname }, 'oncreateclient');
@@ -38,12 +58,34 @@ class Arrange {
     }
 
     /**
-     * Creates an user within the given client.
+     * Creates an user within the client where the currently logged in user belongs to. When the 'admin' user
+     * is logged in, the 'clientid' parameter must be set to define in which client the user should be created.
+     * Fails when there is already an user with the given username in any client.
      * Returns the user without a password but with the generated id.
      * When there is no client with the given name, null is returned.
+     * @param {String} username Username to use. Must be unique across all clients. 'admin' is not allowed.
+     * @param {String} password Password for the new user
+     * @param {String} clientid When the currently logged in user is 'admin', the new user is created in the client with this id, otherwise this parameter is ignored.
+     * @returns {Object} Returns an object with the id of the newly created user or null, when the username is already in use or when the 'admin' user was logged in and there is no client with the given clientid. The password is not returned.
+     * @example
+     * // Create an user in any client when logged in as 'admin'
+     * await arrangeconnection.login('admin', 'adminpassword');
+     * var newuser = await arrangeconnection.createuser('newusername', 'newpassword', 'idoftheclient');
+     * // newuser = {
+     * //    id: 'generatedid',
+     * //    username: 'newusername'
+     * // }
+     * // Login as normal user in a client
+     * await arrangeconnection.login('normalusername', 'userpassword');
+     * // Create an user in the same client as the logged in user
+     * var newuser = await arrangeconnection.createuser('newusername', 'newpassword');
      */
-    async createuser(clientname, username, password) {
-        return this.dorequest('createuser', { clientname: clientname, username: username, password: password }, 'oncreateuser');
+    async createuser(username, password, clientid) {
+        return this.dorequest('createuser', { clientid: clientid, username: username, password: password }, 'oncreateuser');
+    }
+
+    async deleteclient(clientid) {
+        return this.dorequest('deleteclient', { clientid: clientid }, 'ondeleteclient');
     }
 
     /**
@@ -102,14 +144,21 @@ class Arrange {
     }
 
     /**
-     * Login an user
+     * Login an user. Depending on the username the user is logged in at its
+     * corresponding client. Only the 'admin' user has special meanings and functions.
+     * When the user was logged in before, logout() is called automatically, wo that the
+     * user gets re-logged in. When the relogin fails, the user stays in the state "not logged in".
+     * @param {String} username Username to use
+     * @param {String} password Password of the user
+     * @returns {Boolean} True on success, false when the username does not exist or the password is wrong.
      */
     async login(username, password) {
         return this.dorequest('login', { username: username, password: password }, 'onlogin');
     }
 
     /**
-     * Logout the currently logged in user
+     * Logout the currently logged in user by destroying his session. After this call,
+     * only login can be used to relogin. When the user was not logged in before, nothing happens.
      */
     async logout() {
         return this.dorequest('logout', null, 'onlogout');
@@ -127,10 +176,14 @@ class Arrange {
     }
 
     /**
-     * Connect to the server
+     * Connect to the given server.
+     * @param {String} url URL of the server, defaults to window.location at same host
+     * @returns {Arrange} Instance of the arrange server connection
+     * @example
+     * var arrangeconnection = await Arrange.connect('myarrangeserverurl');
      */
-    static async connect() {
-        return new Arrange();
+    static async connect(url) {
+        return new Arrange(url);
     }
 
 }
